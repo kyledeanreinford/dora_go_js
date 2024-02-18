205d37959dc68cf7db5c0611a1261e00417e1358
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2024-02-18 14:02:47 -0600

    Hardcode values

--- backend/server.go
+++ backend/server.go
@@ -1,54 +1,69 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/google/go-github/v58/github"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"os"
 )
 
 type WorkflowRun struct {
-	status       string
-	releaseCount int
-	velocity     string
-	volatility   string
-	workflowId   int64
+	Status       string `json:"Status"`
+	ReleaseCount int `json:"Release Count"`
+	Velocity     string `json:"Velocity"`
+	Volatility   string `json:"Volatility"`
+	WorkflowId   int64 `json:"Workflow ID"`
 }
 
+var WorkflowRuns []WorkflowRun
+
 func getLatestWorkflow(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
-	client := github.NewClient(nil).WithAuthToken(os.Getenv("GH_AUTH"))
-
-	opts := &github.ListWorkflowRunsOptions{Branch: "main"}
-
-	workflowRuns, _, workflowErr := client.Actions.ListRepositoryWorkflowRuns(ctx, "kyledeanreinford", "doraproject", opts)
-
-	if workflowErr != nil {
-		fmt.Printf("\nerror: %v\n", workflowErr)
-		return
-	}
+	//ctx := context.Background()
+	//client := github.NewClient(nil).WithAuthToken(os.Getenv("GH_AUTH"))
+	//
+	//opts := &github.ListWorkflowRunsOptions{Branch: "main"}
+	//
+	//workflowRuns, _, workflowErr := client.Actions.ListRepositoryWorkflowRuns(ctx, "kyledeanreinford", "doraproject", opts)
+	//
+	//if workflowErr != nil {
+	//	fmt.Printf("\nerror: %v\n", workflowErr)
+	//	return
+	//}
 
 	log.Print("Getting latest workflow")
 
-	err := json.NewEncoder(w).Encode(WorkflowRun{
-		status: workflowRuns.WorkflowRuns[0].GetStatus(),
-		releaseCount: workflowRuns.WorkflowRuns[0].GetRunNumber(),
-		velocity: "mildly volatile",
-		volatility: "super fast",
-		workflowId: workflowRuns.WorkflowRuns[0].GetWorkflowID(),
-	})
+	var mockWorkflowRun = WorkflowRun{
+		Status:       "completed",
+		ReleaseCount: 1000,
+		Velocity:     "super fast",
+		Volatility:   "mildly volatile",
+		WorkflowId:   1234,
+	}
+
+	//var workflowRun = WorkflowRun{
+	//	status: workflowRuns.WorkflowRuns[0].GetStatus(),
+	//	releaseCount: workflowRuns.WorkflowRuns[0].GetRunNumber(),
+	//	velocity: "mildly volatile",
+	//	volatility: "super fast",
+	//	workflowId: workflowRuns.WorkflowRuns[0].GetWorkflowID(),
+	//}
+
+	err := json.NewEncoder(w).Encode(mockWorkflowRun)
 	if err != nil {
 		log.Print("Error encoding workflow")
 		return
 	}
 }
 
+func getAllWorkflows(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getAllWorkflows")
+	json.NewEncoder(w).Encode(WorkflowRuns)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", getLatestWorkflow)
+	http.HandleFunc("/workflows", getAllWorkflows)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
@@ -60,5 +75,9 @@ func main() {
 		log.Print(".env file loaded")
 	}
 
+	WorkflowRuns = []WorkflowRun{
+		{"in progress", 123, "fast!", "volatile", 12345},
+		{"completed", 4, "faster!", "more volatile", 555},
+	}
 	handleRequests()
 }
